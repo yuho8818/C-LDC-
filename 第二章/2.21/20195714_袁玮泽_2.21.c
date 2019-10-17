d8714aee4c088cf32aad84add4f979dc9fe004e2
袁玮泽 <56642931+Youder-Yuyucoder@users.noreply.github.com>
2019-10-17 19:26:48 +0800

    Create 20195714_袁玮泽_2.21.c

---
+++ 第二章/2.21/20195714_袁玮泽_2.21.c
@@ -0,0 +1,31 @@
+/*2.19
+ * This program was written by Weize Yuan
+ * Produced date 10/17/2019, Northeastern University,China
+ * 题目：请编写一个用星号（*）打印如下图形的程序
+
+     *********       ***          *            *
+     *       *     *     *       ***          * *
+     *       *    *       *     *****        *   *
+     *       *    *       *       *         *     *
+     *       *    *       *       *        *       *
+     *       *    *       *       *         *     *
+     *       *    *       *       *          *   *
+     *       *     *     *        *           * *
+     *********       ***          *            *
+
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("*********\t   ***   \t    *    \t    *    \n");
+    printf("*       *\t *     * \t   ***   \t   * *   \n");
+    printf("*       *\t*       *\t  *****  \t  *   *  \n");
+    printf("*       *\t*       *\t    *    \t *     * \n");
+    printf("*       *\t*       *\t    *    \t*       *\n");
+    printf("*       *\t*       *\t    *    \t *     * \n");
+    printf("*       *\t*       *\t    *    \t  *   *  \n");
+    printf("*       *\t *     * \t    *    \t   * *   \n");
+    printf("*********\t   ***   \t    *    \t    *    \n");
+return 0;
+}
